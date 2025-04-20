cd3687fd58e472daa55e3e4820563d3d5ae47097
tksyksk <143901071+qmkvqw@users.noreply.github.com>
2025-04-20 23:55:31 +0300

    Добавьте файлы проекта.

---
+++ Controllers/MastersController.cs
@@ -0,0 +1,21 @@
+﻿using BeautySalonApi.Services.MastersService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeautySalonApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MastersController : Controller
+    {
+        private readonly IMastersService _service;
+        public MastersController(IMastersService service)
+        {
+            _service = service;
+        }
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(_service.GetAll());
+        }
+    }
+}
